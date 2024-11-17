f7e31d07a6851f208ece32571a1a68a1a78435a1
unknown <dianaa140844@gmail.com>
2024-11-17 11:30:32 +0700

    อัปไฟล์Admin

---
+++ Admin/menu.php
@@ -0,0 +1,12 @@
+<div class="cal-12">
+    <div> <a href="./index.php">หน้าหลัก</a></div>
+    <div> <a href="./sale_admin.php">หน้าขาย</a></div>
+    <div> <a href="./buy_admin.php">รับซื้อสินค้า</a></div>
+    <div> <a href="./product_admin.php">ข้อมูลของเก่า</a></div>
+    <div> <a href="./stock_admin.php">สต๊อก</a></div>
+    <div> <a href="./report_admin.php">รายงาน</a></div>
+    <div> <a href="./staff_admin.php">ข้อมูลพนักงาน</a></div>
+    <div> <a href="./expense_staff.php">ค่าแรงพนักงาน</a></div>
+    <div> <a href="./checktimes_staff.php">เช็คการเข้า-ออกของพนักงาน</a></div>
+    <div> <a href="">logout</a></div>
+</div>
