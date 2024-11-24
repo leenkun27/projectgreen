6ab33ee565524a862d3da9de97b05c0f050948b4
leenkun27 <greennylove.p@gmail.com>
2024-11-24 17:05:57 +0700

    เชื่อมฐานข้อมูล

---
+++ condb.php
@@ -0,0 +1,22 @@
+<?php
+$host = 'localhost';
+$username = 'root';
+$password = '';
+$dbname = 'wichian_db';
+
+$conn = new mysqli($host, $username, $password);
+
+// ตรวจสอบการเชื่อมต่อ
+if ($conn->connect_error) {
+    die("การเชื่อมต่อเซิร์ฟเวอร์ผิดพลาด: " . $conn->connect_error);
+}
+
+// ตรวจสอบว่าฐานข้อมูลมีอยู่จริงหรือไม่
+$db_selected = $conn->select_db($dbname);
+if (!$db_selected) {
+    die("ไม่พบฐานข้อมูล: $dbname");
+}
+
+echo "เชื่อมต่อสำเร็จ!";
+?>
+
