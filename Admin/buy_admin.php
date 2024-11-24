ea0fcbe4573d52e5c7d2153510501c5089b420c0
unknown <dianaa140844@gmail.com>
2024-11-24 17:09:06 +0700

    แก้ไขหน้าbuy_admin

--- Admin/buy_admin.php
+++ Admin/buy_admin.php
@@ -21,13 +21,14 @@
             <div class="col-10">
                 <h1>รับซื้อของเก่า</h1>
                 <div class="row mt-5">
-                    <div class="col-lg-3 col-md-4 col-sm-6">
+                    <div class="col-lg-6 col-md-4 col-sm-6">
                         ชื่อของเก่า
                         <div class="input-group">
                             <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
                         </div>
-                        <br>
-                        ประเภทของเก่า
+                    </div>
+                    <div class="col-6">
+                        <div>ประเภทของเก่า</div>
                         <div class="input-group">
                             <select class="form-control" id="" placeholder="">
                                 <option>เศษเหล็ก</option>
@@ -39,103 +40,83 @@
                                 <option>อื่นๆ</option>
                             </select>
                         </div>
-                        <br>
-                        ปริมาณการรับซื้อ
+                    </div>
+                    <div class="col-6 mt-2">
+                        <div>ปริมาณการรับซื้อ</div>
                         <div class="input-group">
                             <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="">
                         </div>
                     </div>
-                </div>
+               
 
-                <div class="row mt-5">
-                    <div class="col-6">ชื่อของเก่า
-                        <div class="input-group">
-                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
-                        </div></div>
-                    <div class="col-6">ชื่อของเก่า
-                        <div class="input-group">
-                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
-                        </div></div>
-                </div>
-                <div class="row mt-5">
-                    <div class="col-6">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://th.bing.com/th/id/OIP.iZ6lxNXuEqjJd8FcnrFIygHaHa?w=1000&h=1000&rs=1&pid=ImgDetMain" alt="">
-                            <div class="card-body">
-                                <h3 class="card-title">กระดาษ</h3>
-                                <p class="class-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, eum?</p>
-                            </div>
-                        </div>
+                <div class="col-6 mt-2">
+                    <div>ชื่อของเก่า</div>
+                    <div class="input-group">
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
                     </div>
-                    <div class="col-6">
-                        <div class="card" style="width: 18rem;">
-                            <img src="https://th.bing.com/th/id/OIP.iZ6lxNXuEqjJd8FcnrFIygHaHa?w=1000&h=1000&rs=1&pid=ImgDetMain" alt="">
-                            <div class="card-body">
-                                <h3 class="card-title">กระดาษ</h3>
-                                <p class="class-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquam, eum?</p>
-                                <div class="row">
-                                    <div class="col-6"><button class="btn btn-sm btn-warning cart"><i class="bi bi-cart"></i></button></div>
-                                    <div class="col-6"><button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                                            ดูรายละเอียด
-                                        </button></div>
-                                </div>
-                            </div>
-                        </div>
+                </div>
+                <div class="col-6 mt-2">
+                    <div>ชื่อของเก่า</div>
+                    <div class="input-group">
+                        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="">
                     </div>
                 </div>
 
-
+                </div>
             </div>
+
+
         </div>
+    </div>
 
 
 
 
 
-        <!-- Modal -->
-        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="exampleModalLabel">กระดาษ</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">กระดาษ</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="d-flex justify-content-center">
+                        <img
+                            class="w-50"
+                            src="https://th.bing.com/th/id/OIP.iZ6lxNXuEqjJd8FcnrFIygHaHa?w=1000&h=1000&rs=1&pid=ImgDetMain"
+                            alt="">
                     </div>
-                    <div class="modal-body">
-                        <div class="d-flex justify-content-center">
-                            <img
-                                class="w-50"
-                                src="https://th.bing.com/th/id/OIP.iZ6lxNXuEqjJd8FcnrFIygHaHa?w=1000&h=1000&rs=1&pid=ImgDetMain"
-                                alt="">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputEmail1" class="form-label">จำนวน</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-                            <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-                        </div>
-                        <div class="mb-3">
-                            <input type="text" class="form-control">
-                        </div>
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">จำนวน</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                        <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                     </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
-                        <button type="button" class="btn btn-success">บันทึก</button>
+                    <div class="mb-3">
+                        <input type="text" class="form-control">
                     </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">ปิด</button>
+                    <button type="button" class="btn btn-success">บันทึก</button>
                 </div>
             </div>
         </div>
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-        <script>
-            $(document).ready(function() {
-                $(".cart").click(function() {
-                    Swal.fire({
-                        title: "สำเร็จ",
-                        text: "You clicked the button!",
-                        icon: "success"
-                    });
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        $(document).ready(function() {
+            $(".cart").click(function() {
+                Swal.fire({
+                    title: "สำเร็จ",
+                    text: "You clicked the button!",
+                    icon: "success"
                 });
             });
-        </script>
+        });
+    </script>
 </body>
 
 </html>
